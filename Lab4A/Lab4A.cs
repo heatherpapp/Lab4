06056b94c2bb1e4f6fff84cdd63bd93345d328d4
Heather Papp <112786769+heatherpapp@users.noreply.github.com>
2022-09-12 02:29:24 +0000

    added lab files from VS

---
+++ Lab4A/Lab4A.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Class: CSE 1321L
+Section: W1 C#
+Term: Fall 2022	
+Instructor: Jaskirat Singh Sohal
+Name: Heather Papp
+Lab#: Lab4A 
+*/
+namespace Lab4A
+{
+    internal class Lab4A
+    {
+        static void Main(string[] args)
+        {                               
+            float points;            
+            
+
+            Console.Write("Enter the score of your exam: ");
+            points = float.Parse(Console.ReadLine());
+            
+
+            {   string grade;
+                grade = "";
+                                
+                if ((points >=98.0) && (points <= 100.0))
+                    grade = "A+";
+                
+                else if ((points >= 95.0) && (points <= 97.9))
+                    grade = "A";
+                
+                else if ((points >= 92.0) && (points <= 94.9))
+                    grade = "A-";
+               
+                else if ((points >= 89.0) && (points <= 91.9))
+                    grade = "B+";
+               
+                else if ((points >= 86.0) && (points <= 88.9))
+                    grade = "B";
+                
+                else if ((points >= 83.0) && (points <= 85.9))
+                    grade = "B-";
+                
+                else if ((points >= 80.0) && (points <= 82.9))
+                    grade = "C+";
+                
+                else if ((points >= 77.0) && (points <= 79.9))
+                    grade = "C";
+                
+                else if ((points >= 74.0) && (points <= 76.9))
+                    grade = "C-";
+                
+                else if ((points >= 71.0) && (points <= 73.9))
+                    grade = "D";
+                
+                else if ((points >= 65.0) && (points <= 67.9))
+                    grade = "D-";
+                
+                else if ((points >= 0.0) && (points <= 64.9))
+                    grade = "F";
+                                              
+                Console.WriteLine("Letter grade is: " +grade+ "");
+
+                
+            }        
+        
+        }
+        
+    }
+}
---
+++ Lab4C/Lab4C.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Class: CSE 1321L
+Section: W1 C#
+Term: Fall 2022	
+Instructor: Jaskirat Singh Sohal
+Name: Heather Papp
+Lab#: Lab4C 
+*/
+namespace Lab4C
+{
+    internal class Lab4C
+    {
+        static void Main(string[] args)
+        {           
+            Console.Write("Welcome\nPlease input a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What would you like to do to this number:\n"
+                            + "0 - Get the additive inverse of the number\n"
+                            + "1 - Get the reciprocal of the number\n"
+                            + "2 - Square the number\n"
+                            + "3 - Cube the number\n"
+                            + "4 - Exit the program\r\n");
+            int selection = int.Parse(Console.ReadLine());
+                            
+            switch (selection)
+            {
+                case 0:
+                    Console.WriteLine("The additive inverse of " + userInput + " is " (+userInput - 2*userInput+));
+                    break;
+
+                case 1:
+                    Console.WriteLine("The reciprocal of {0:0.0###} is {1:0.0###}", userInput, 1/userInput);
+                    break;
+
+                case 2:
+                    Console.WriteLine("The square of {0:0.0###} is {1:0.0###}", userInput, userInput*userInput);
+                    break;
+
+                case 3:
+                    Console.WriteLine("The cube of {0:0:0###} is {1:0.0###}", userInput, userInput*userInput*userInput);
+                    break;
+
+                case 4:
+                    Console.WriteLine("Thank you, goodbye!");
+                    break;
+                default:
+                Console.WriteLine("Invalid input, please try again!");
+                    break;
+
+                }
+            
+        }
+    }
+}
